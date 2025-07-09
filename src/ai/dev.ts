8489fc4072ab6f627ce7437c3e0b82e48100e3ce
RushangChandekar <142429467+rushangchandekar@users.noreply.github.com>
2025-07-09 22:42:59 +0530

    commit

--- src/ai/dev.ts
+++
@@ -1,5 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/generate-recipe.ts';
-import '@/ai/flows/improve-recipes.ts';
--- src/components/theme-provider.tsx
+++
@@ -1,8 +0,0 @@
-// components/theme-provider.tsx
-'use client';
-
-import { ReactNode } from "react";
-
-export function ThemeProvider({ children }: { children: ReactNode }) {
-  return <>{children}</>;
-}
